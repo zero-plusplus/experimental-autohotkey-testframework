906cb634db286fd6b904e7af284b3a3f910fbbe9
zero-plusplus <git@zero-plusplus.com>
2021-09-07 20:13:07 +0900

    Add tasks

--- gulpfile.ts
+++ gulpfile.ts
@@ -7,10 +7,28 @@ const buildDir = './build';
 export const clean = async(): Promise<void> => {
   await del(buildDir);
 };
-export const build_v2 = (): void => {
+export const build_v2 = async(): Promise<void> => {
   const ahkVersion_2 = 2;
   const inliner_v2 = new AhkInliner(ahkVersion_2);
   mkdirp.sync(buildDir);
   inliner_v2.write('./src/UnitTest/UnitTest.ahk2', './build/UnitTest.ahk2');
+
+  return Promise.resolve();
 };
 export const build = gulp.series(clean, build_v2);
+
+export const watch = async(): Promise<void> => {
+  const watchTask = async(): Promise<void> => {
+    await gulp.series(
+      async function taskStart(): Promise<void> {
+        return Promise.resolve();
+      },
+      gulp.parallel(build),
+      async function taskDone(): Promise<void> {
+        return Promise.resolve();
+      },
+    )(() => '');
+  };
+  gulp.watch([ 'src/**/*.*', 'src/**/.*.*' ], { ignoreInitial: false }, watchTask);
+  return Promise.resolve();
+};
